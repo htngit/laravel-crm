13b1830f35666de5e82bbc7a55a78a1b5f6b99a1
andry <htn.git@gmail.com>
2025-02-15 03:55:13 +0700

    sip

--- packages/Webkul/Admin/src/Resources/views/components/layouts/sidebar/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/layouts/sidebar/index.blade.php
@@ -10,16 +10,25 @@
             @foreach (menu()->getItems('admin') as $menuItem)
                 <div class="px-4 group/item {{ $menuItem->isActive() ? 'active' : 'inactive' }}">
                     <a
-                        class="flex gap-2 p-1.5 items-center cursor-pointer hover:rounded-lg {{ $menuItem->isActive() == 'active' ? 'bg-brandColor rounded-lg' : ' hover:bg-gray-100 hover:dark:bg-gray-950' }} peer"
+                        class="flex gap-2 p-1.5 items-center cursor-pointer hover:rounded-lg"
+                        :class="{
+                            'bg-brandColor rounded-lg': '{{ $menuItem->isActive() }}' === 'active',
+                            'hover:bg-gray-100 hover:dark:bg-gray-950': '{{ $menuItem->isActive() }}' !== 'active'
+                        }"
                         href="{{ ! in_array($menuItem->getKey(), ['settings', 'configuration']) && $menuItem->haveChildren() ? 'javascript:void(0)' : $menuItem->getUrl() }}"
-                        :data-menu-key="'{{ $menuItem->getKey() }}'"
-                        @mouseover="hoveringMenu = $event.target.closest('a').dataset.menuKey"
-                        @mouseleave="!isMenuActive ? hoveringMenu = '' : null"
+                        data-menu-key="{{ $menuItem->getKey() }}"
+                        @mouseenter="hoveringMenu = '{{ $menuItem->getKey() }}'"
+                        @mouseleave="hoveringMenu = ''"
                         @click="isMenuActive = !isMenuActive"
                     >
                         <span class="{{ $menuItem->getIcon() }} text-2xl {{ $menuItem->isActive() ? 'text-white' : ''}}"></span>
 
-                        <div class="flex-1 flex justify-between items-center text-gray-600 dark:text-gray-300 font-medium whitespace-nowrap group-[.sidebar-collapsed]/container:hidden {{ $menuItem->isActive() ? 'text-white' : ''}} group">
+                        <div
+                            class="flex-1 flex justify-between items-center font-medium whitespace-nowrap group-[.sidebar-collapsed]/container:hidden group"
+                            :class="{
+                                'text-white': '{{ $menuItem->isActive() }}' === 'active',
+                                'text-gray-600 dark:text-gray-300': '{{ $menuItem->isActive() }}' !== 'active'
+                            }"
                             <p>{{ core()->getConfigData('general.settings.menu.'.$menuItem->getKey()) ?? $menuItem->getName() }}</p>
                         
                             @if ( ! in_array($menuItem->getKey(), ['settings', 'configuration']) && $menuItem->haveChildren())
@@ -34,8 +43,8 @@
                         && $menuItem->haveChildren()
                     )
                         <div
-                            class="absolute top-0 hidden flex-col bg-gray-100 ltr:left-[200px] rtl:right-[199px]"
-                            :class="[isMenuActive && (hoveringMenu === $el.previousElementSibling.dataset.menuKey) ? '!flex' : 'hidden']"
+                            class="absolute top-0 flex-col bg-gray-100 ltr:left-[200px] rtl:right-[199px]"
+                            v-show="isMenuActive && hoveringMenu === '{{ $menuItem->getKey() }}'"
                         >
                             <div class="sidebar-rounded fixed z-[1000] h-full min-w-[140px] max-w-max bg-white pt-4 after:-right-[30px] dark:border-gray-800 dark:bg-gray-900 max-lg:hidden ltr:border-r rtl:border-x">
                                 <div class="journal-scroll h-[calc(100vh-100px)] overflow-hidden">
