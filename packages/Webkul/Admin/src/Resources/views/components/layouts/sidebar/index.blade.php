fa43e1ad913fe98812b6099aacaf090695bf0017
andry <htn.git@gmail.com>
2025-02-15 04:16:00 +0700

    okwe

--- packages/Webkul/Admin/src/Resources/views/components/layouts/sidebar/index.blade.php
+++ packages/Webkul/Admin/src/Resources/views/components/layouts/sidebar/index.blade.php
@@ -15,13 +15,19 @@
                             'bg-brandColor rounded-lg': '{{ $menuItem->isActive() }}' === 'active',
                             'hover:bg-gray-100 hover:dark:bg-gray-950': '{{ $menuItem->isActive() }}' !== 'active'
                         }"
-                        href="{{ ! in_array($menuItem->getKey(), ['settings', 'configuration']) && $menuItem->haveChildren() ? 'javascript:void(0)' : $menuItem->getUrl() }}"
+                        :href="'{{ ! in_array($menuItem->getKey(), ['settings', 'configuration']) && $menuItem->haveChildren() }}' === '1' ? 'javascript:void(0)' : '{{ $menuItem->getUrl() }}'"
                         data-menu-key="{{ $menuItem->getKey() }}"
                         @mouseenter="hoveringMenu = '{{ $menuItem->getKey() }}'"
                         @mouseleave="hoveringMenu = ''"
                         @click="isMenuActive = !isMenuActive"
                     >
-                        <span class="{{ $menuItem->getIcon() }} text-2xl {{ $menuItem->isActive() ? 'text-white' : ''}}"></span>
+                        <span
+                            :class="[
+                                '{{ $menuItem->getIcon() }}',
+                                'text-2xl',
+                                {'text-white': '{{ $menuItem->isActive() }}' === 'active'}
+                            ]"
+                        ></span>
 
                         <div
                             class="flex-1 flex justify-between items-center font-medium whitespace-nowrap group-[.sidebar-collapsed]/container:hidden group"
@@ -29,35 +35,44 @@
                                 'text-white': '{{ $menuItem->isActive() }}' === 'active',
                                 'text-gray-600 dark:text-gray-300': '{{ $menuItem->isActive() }}' !== 'active'
                             }"
-                            <p>{{ core()->getConfigData('general.settings.menu.'.$menuItem->getKey()) ?? $menuItem->getName() }}</p>
-                        
-                            @if ( ! in_array($menuItem->getKey(), ['settings', 'configuration']) && $menuItem->haveChildren())
-                                <i class="icon-right-arrow rtl:icon-left-arrow invisible text-2xl group-hover/item:visible {{ $menuItem->isActive() ? 'text-white' : ''}}"></i>
-                            @endif
+                        >
+                            <span class="menu-text">{{ core()->getConfigData('general.settings.menu.'.$menuItem->getKey()) ?? $menuItem->getName() }}</span>
+                            <i
+                                v-if="'{{ ! in_array($menuItem->getKey(), ['settings', 'configuration']) && $menuItem->haveChildren() }}'"
+                                class="icon-right-arrow rtl:icon-left-arrow invisible text-2xl group-hover/item:visible"
+                                :class="{'text-white': '{{ $menuItem->isActive() }}' === 'active'}"
+                            ></i>
                         </div>
                     </a>
 
                     <!-- Submenu -->
-                    @if (
-                        ! in_array($menuItem->getKey(), ['settings', 'configuration'])
-                        && $menuItem->haveChildren()
-                    )
-                        <div
-                            class="absolute top-0 flex-col bg-gray-100 ltr:left-[200px] rtl:right-[199px]"
-                            v-show="isMenuActive && hoveringMenu === '{{ $menuItem->getKey() }}'"
-                        >
+                    <div
+                        v-if="'{{ ! in_array($menuItem->getKey(), ['settings', 'configuration']) && $menuItem->haveChildren() }}'"
+                        class="absolute top-0 flex-col bg-gray-100 ltr:left-[200px] rtl:right-[199px]"
+                        v-show="isMenuActive && hoveringMenu === '{{ $menuItem->getKey() }}'"
+                    >
                             <div class="sidebar-rounded fixed z-[1000] h-full min-w-[140px] max-w-max bg-white pt-4 after:-right-[30px] dark:border-gray-800 dark:bg-gray-900 max-lg:hidden ltr:border-r rtl:border-x">
                                 <div class="journal-scroll h-[calc(100vh-100px)] overflow-hidden">
                                     <nav class="grid w-full gap-2">
                                         @foreach ($menuItem->getChildren() as $subMenuItem)
                                             <div class="px-4 group/item {{ $menuItem->isActive() ? 'active' : 'inactive' }}">
                                                 <a
-                                                    href="{{ $subMenuItem->getUrl() }}"
-                                                    class="flex gap-2.5 p-2 items-center cursor-pointer hover:rounded-lg {{ $subMenuItem->isActive() == 'active' ? 'bg-brandColor rounded-lg' : ' hover:bg-gray-100 hover:dark:bg-gray-950' }} peer"
+                                                    :href="'{{ $subMenuItem->getUrl() }}'"
+                                                    class="flex gap-2.5 p-2 items-center cursor-pointer hover:rounded-lg peer"
+                                                    :class="{
+                                                        'bg-brandColor rounded-lg': '{{ $subMenuItem->isActive() }}' === 'active',
+                                                        'hover:bg-gray-100 hover:dark:bg-gray-950': '{{ $subMenuItem->isActive() }}' !== 'active'
+                                                    }"
                                                 >
-                                                    <p class="text-gray-600 dark:text-gray-300 font-medium whitespace-nowrap {{ $subMenuItem->isActive() ? 'text-white' : ''}}">
+                                                    <span
+                                                        class="font-medium whitespace-nowrap"
+                                                        :class="{
+                                                            'text-white': '{{ $subMenuItem->isActive() }}' === 'active',
+                                                            'text-gray-600 dark:text-gray-300': '{{ $subMenuItem->isActive() }}' !== 'active'
+                                                        }"
+                                                    >
                                                         {{ core()->getConfigData('general.settings.menu.'.$subMenuItem->getKey()) ?? $subMenuItem->getName() }}
-                                                    </p>
+                                                    </span>
                                                 </a>
                                             </div>
                                         @endforeach
@@ -65,7 +80,6 @@
                                 </div>
                             </div>
                         </div>
-                    @endif
                 </div>
             @endforeach
         </nav>
